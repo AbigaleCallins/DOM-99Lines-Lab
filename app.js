89747d07918d012fc8732d493eac86b552ca6197
AbigaleCallins <alcallins@gmail.com>
2022-02-15 11:49:28 -0600

    final

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
-let friends = ["Micheal", "Kendra", "Paula", "Brad", "Josie"];
-//loop to use each friend counting down
+document.addEventListener('DOMContentLoaded', function () {
 
-//wanted to recreate this lab using the format we are learning now in class
+  let btn = document.createElement("button");
+  btnText = document.createTextNode("Sing!!!");
+  btn.appendChild(btnText)
+  document.body.appendChild(btn);
 
-for (let i = 0; i < friends.length; i++) {
-  console.log(friends[i].toUpperCase);
-  //for every friend loop, the countD loop must run 100x.
-  // -- sub 1; index --> j
-  for (let countD = 99; countD > 0; countD--) {
-    if (countD > 1) {
-      console.log(
-        countD +
-          " line of code in the file, " +
-          countD +
-          " line of code, " +
-          friends[i] +
-          " strikes one out, clears it all out, no more lines of code in the file."
-      );
-    } else if (i === 2) {
-      let oneLess = countD - 1;
-      console.log(
-        countD +
-          " lines of code in the file, " +
-          countD +
-          " lines of code, " +
-          friends[i] +
-          " strikes one out, clears it all out, " +
-          oneLess +
-          " line of code in the file, "
-      );
-    } else {
-      let oneLess = oneLeft - 1;
-      console.log(
-        countD +
-          " lines of code in the file, " +
-          oneLeft +
-          " lines of code, " +
-          friends[i] +
-          " strikes one out, clears it all out, " +
-          oneLess +
-          " lines of code in the file."
-      );
+  btn.addEventListener('click', function () {
+    let friends = ["Abbie", "Lisa", "Paula", "Brad", "Josie"];
+    for (let j = 0; j < friends.length; j++) {
+      let friendDiv = document.createElement("div");
+      friendDiv.className = ("friend");
+      document.body.appendChild(friendDiv);
+
+      let friendNameH2 = document.createElement("h2");
+      let h2Text = document.createTextNode(friends[j]);
+      friendNameH2.appendChild(h2Text)
+      friendDiv.appendChild(friendNameH2)
+      friendDiv.innerHTML = ("<h2 class='name'>" + friends[j].toUpperCase() + "</h2>");
+
+      // let song = document.createElement("p")
+      // let songText = document.createTextNode(
+
+      // // song.innerText(friends[countD].toUpperCase());
+
+
+      for (let i = 99; i > 0; i--) {
+
+        if (i >= 2) {
+          let p = document.createElement("p")
+          p.innerText = (i + " lines of code in the file, " + i + " lines of code, " + friends[j] + " strikes one out, clears it all out, " + i + " lines of code in the file, ");
+          friendDiv.appendChild(p)
+        } else if (i == 1) {
+          let p = document.createElement("p")
+          p.innerText = (i + " line of code in the file, " + i + " line of code, " + friends[j] + " strikes one out, clears it all out, no more lines of code in the file.");
+          friendDiv.appendChild(p)
+        } else {
+          let p = document.createElement("p")
+          p.innerText = (i + " lines of code in the file, " + i + " lines of code, " + friends[j] + " strikes one out, clears it all out, " + i + " lines of code in the file.");
+          friendDiv.appendChild(p)
+        }
+
+
+      }
     }
-  }
-}
+
+  })
+})
